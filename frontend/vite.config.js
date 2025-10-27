99ed89a63faaf30efcb2529baaa96ef2fa6ffc0c
green-1-three <freemanjamesh1989@gmail.com>
2025-10-27 16:24:19 -0400

    stop minifying in frontend vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,6 @@
 export default {
     build: {
         sourcemap: true,
-        minify: 'esbuild',
-        target: 'es2015'
-    },
-    esbuild: {
-        keepNames: true  // Prevents mangling of class names like Map
+        minify: false  // Disable minification - it breaks Map cache
     }
 };
