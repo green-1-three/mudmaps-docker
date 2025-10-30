eee22436bf1a44597d29221d0c7a93e97354012b
green-1-three <freemanjamesh1989@gmail.com>
2025-10-30 14:18:29 -0400

    update vit config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,15 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
     build: {
         sourcemap: true,
-        minify: false  // Disable minification - it breaks Map cache
+        minify: false,  // Disable minification - it breaks Map cache
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dev: resolve(__dirname, 'dev.html')
+            }
+        }
     }
-};
+});
